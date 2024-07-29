5fe47339db609dd574df3e04224e67aa8431a599
MatteoTerzaghi <matteo@glassfy.io>
2024-07-29 12:16:17 +0200

    responsive hp

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,8 @@ export default function Card({
 }) {
   return (
     <div
-      className="bg-white p-10 border col-start-4 col-span-6 rounded-3xl shadow-lg"
+      // className="xl:bg-red-400 lg:bg-green-400 md:bg-pink-400 sm:bg-blue-400 bg-white p-10 border col-start-4 col-span-6 rounded-3xl shadow-lg"
+      className="bg-white col-span-full sm:col-span-10 sm:col-start-2 sm:rounded-3xl sm:shadow-lg sm:border sm:p-10 xl:col-span-8 xl:col-start-3"
       style={{ minHeight: `${minHeight ? `${minHeight}px` : "0px"}` }}
     >
       {children}
