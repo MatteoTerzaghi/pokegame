882d6c8b8e6a4ff5daccbe970fb630f9daec526f
MatteoTerzaghi <matteo@glassfy.io>
2024-07-30 09:08:35 +0200

    whos that pokemon string similarity

--- src/app/whosthatpokemon/page.tsx
+++ src/app/whosthatpokemon/page.tsx
@@ -46,6 +46,37 @@ export default function WhoIsThatPokemon() {
     getRandomPokemon();
   }, []);
 
+  function levenshteinDistance(a: string, b: string): number {
+    const dp: number[][] = [];
+
+    for (let i = 0; i <= a.length; i++) {
+      dp[i] = [i];
+    }
+    for (let j = 1; j <= b.length; j++) {
+      dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= a.length; i++) {
+      for (let j = 1; j <= b.length; j++) {
+        const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+        dp[i][j] = Math.min(
+          dp[i - 1][j] + 1, // Deletion
+          dp[i][j - 1] + 1, // Insertion
+          dp[i - 1][j - 1] + cost // Substitution
+        );
+      }
+    }
+
+    return dp[a.length][b.length];
+  }
+
+  function similarityPercentage(str1: string, str2: string): number {
+    const distance = levenshteinDistance(str1, str2);
+    const maxLength = Math.max(str1.length, str2.length);
+    const similarity = ((maxLength - distance) / maxLength) * 100;
+    return similarity;
+  }
+
   return (
     <div className="grid grid-cols-12 pt-20">
       <Card>
@@ -130,7 +161,15 @@ export default function WhoIsThatPokemon() {
                           onClick={() => {
                             if (
                               nameGuessed.toLowerCase() ===
-                              pokemon?.name.replaceAll("-", " ").toLowerCase()
+                                pokemon?.name
+                                  .replaceAll("-", " ")
+                                  .toLowerCase() ||
+                              similarityPercentage(
+                                nameGuessed.toLowerCase(),
+                                pokemon?.name
+                                  .replaceAll("-", " ")
+                                  .toLowerCase() ?? ""
+                              ) >= 80
                             ) {
                               const newCurrentStreak = currentStreak + 1;
                               changeCurrentStreak(newCurrentStreak);
